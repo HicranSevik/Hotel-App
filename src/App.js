a2727b170873bf0fa28a7e68098724da679e95e6
Hicran Sevik <hicran_sevik@outlook.com>
2020-01-27 21:57:04 -0800

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Components
+import SignUpForm from './pages/SignUpForm';
+import SignInForm from './pages/SignInForm';
+import FindHotel from './pages/FindHotel';
+import AddHotel from './pages/AddHotel';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={SignUpForm}>
+          </Route>
+          <Route path="/sign-in" component={SignInForm}>
+          </Route>
+          <Route path="/find-hotel" component={FindHotel}>
+          </Route>
+          <Route path="/add-hotel" component={AddHotel}>
+          </Route>
+        </div>
+      </Router >
+    )
+  }
+
 }
 
 export default App;
